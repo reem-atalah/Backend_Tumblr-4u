3e8d3539ef321f2ed28a6d4390482577a99b89e0
ahmedayman1420 <76254195+ahmedayman1420@users.noreply.github.com>
2021-12-10 01:11:42 -0800

    test

--- test/userTest.js
+++ test/userTest.js
@@ -18,6 +18,8 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../script');
+const userServices = require('../Modules/Users/Controller/services');
+const { expect } = require('chai');
 
 // const connect = require('../Configurations/configuration');
 
@@ -36,171 +38,33 @@ chai.use(chaiHttp);
 /* =======================================*/
 
 describe('User APIs', () => {
-  /*
+/*
  ///////// <---------> ======== <---------> ///////// =====================>
- ///////// <---------> Sign Up <---------> ///////// =====================>
+ ///////// <---------> Check Mail <---------> ///////// =====================>
  ///////// <---------> ======== <---------> ///////// =====================>
-    */
+*/
 
-  describe('User /signup', () => {
+  describe('Check Mail Function', () => {
     // ----------------// <=====> 1-Case <=====> //----------------//
-    it('It Should Create New User', (done) => {
-      const user = {
-        email: 'Ayman023@gmail.com',
-        password: '123',
-        blogName: 'CMP-023',
-        age: '21',
-        city: 'Giza',
-        country: 'Egypt',
-      };
-
-      chai
-          .request(server)
-          .post('/signup')
-          .send(user)
-          .end((err, res) => {
-            res.should.have.status(201);
-            res.body.should.be.a('object');
-            res.body.should.have
-                .property('res')
-                .have.property('message')
-                .eq('Sign Up Successfully (<:>)');
-
-            done();
-          });
+    it('It Should Return True', (done) => {
+       const email = 'ahmed0@gmail.com';
+       userServices.checkMail(email).then(result=>{
+        expect(result).to.be.eq(true);
+        done();
+       }).catch(done);
     });
 
-    // ----------------// <=====> 2-Case <=====> //----------------//
-    it('It Should Return Email is Already Exists', (done) => {
-      const user = {
-        email: 'ahmed@gmail.com',
-        password: '123',
-        blogName: 'CMP-100',
-        age: '21',
-        city: 'Giza',
-        country: 'Egypt',
-      };
+    it('It Should Return False', (done) => {
+      const email = 'Afnan@gmail.com';
+      userServices.checkMail(email).then(result=>{
+       expect(result).to.be.eq(false);
+       done();
+      }).catch(done);
+   });
 
-      chai
-          .request(server)
-          .post('/SignUp')
-          .send(user)
-          .end((err, res) => {
-            res.should.have.status(400);
-            res.body.should.be.a('object');
-            res.body.should.have
-                .property('res')
-                .have.property('error')
-                .eq('Email is Already Exists (<:>)');
-            done();
-          });
-    });
-
-    // ----------------// <=====> 3-Case <=====> //----------------//
-    it('It Should Return Blog Name is Already Exists (<:>)', (done) => {
-      const user = {
-        email: 'Omr@gmail.com',
-        password: '123',
-        blogName: 'CMP-013',
-        age: '21',
-        city: 'Giza',
-        country: 'Egypt',
-      };
-
-      chai
-          .request(server)
-          .post('/SignUp')
-          .send(user)
-          .end((err, res) => {
-            res.should.have.status(400);
-            res.body.should.be.a('object');
-            res.body.should.have
-                .property('res')
-                .have.property('error')
-                .eq('Blog Name is Already Exists (<:>)');
-            done();
-          });
-    });
   });
 
-  /*
-    ================ ///////// <---------> ======== <---------> /////////
-    =====>
-    ================ ///////// <---------> Log In <---------> ///////// ==
-    ==>
-    =============== ///////// <---------> ======== <---------> /////////
-    ====>
-    */
-
-  describe('User /login', () => {
-    // ----------------// <=====> 1-Case <=====> //----------------//
-    it('It Should Login User', (done) => {
-      const user = {
-        email: 'ahmed@gmail.com',
-        password: '123',
-      };
-
-      chai
-          .request(server)
-          .post('/login')
-          .send(user)
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have
-                .property('res')
-                .have.property('message')
-                .eq('LogIn Successfully (<:>)');
-
-            done();
-          });
-    });
-
     // ----------------// <=====> 2-Case <=====> //----------------//
-    it('It Should Incorrect Password', (done) => {
-      const user = {
-        email: 'ahmed@gmail.com',
-        password: '1234',
-      };
 
-      chai
-          .request(server)
-          .post('/login')
-          .send(user)
-          .end((err, res) => {
-            res.should.have.status(400);
-            res.body.should.be.a('object');
-            res.body.should.have
-                .property('res')
-                .have.property('error')
-                .eq('InCorrect Password (<:>)');
-
-            done();
-          });
-    });
-
-    // ----------------// <=====> 3-Case <=====> //----------------//
-    it('It Should get Email Is Not Found', (done) => {
-      const user = {
-        email: 'Omr@gmail.com',
-        password: '123',
-      };
-
-      chai
-          .request(server)
-          .post('/login')
-          .send(user)
-          .end((err, res) => {
-            res.should.have.status(400);
-            res.body.should.be.a('object');
-            res.body.should.have
-                .property('res')
-                .have.property('error')
-                .eq('Email Is Not Found (<:>)');
-
-            done();
-          });
-    });
   });
-});
 /* =========== /// <==> End <==> ===========*/
