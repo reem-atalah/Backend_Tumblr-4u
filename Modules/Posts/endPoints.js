d512fe9273b5b4ee34e5e56f4a3c14e8f07076f1
Abeerhbadr <abeer.h.badr@gmail.com>
2021-12-10 16:26:35 +0200

    post apis isAutherized

--- Modules/Posts/endPoints.js
+++ Modules/Posts/endPoints.js
@@ -4,24 +4,34 @@
 
 /* =================== <-- Posts Module EndPoints --> =================== */
 const createPost = 'Posts: createPost';
+const showPost = 'Posts: showPost';
 const editPost = 'Posts: editPost';
 const deletePost = 'Posts: deletePost';
-const likePost = 'Posts: likePost';
-const commentPost = 'Posts: commentPost';
+const likePress = 'Posts: likePress';
+const makeComment = 'Posts: makeComment';
 const shareWith = 'Posts: shareWith';
 const reblogPost = 'Posts: reblogPost';
+const removeComment = 'Posts: removeComment';
+const removeReblog = 'Posts: removeReblog';
+const getNotes = 'Posts: getNotes';
+const getDashboard = 'Posts: getDashboard';
 // const Get_Posts = 'Post:Get_Posts';
 // const Get_All_Posts = 'Post:Get_All_Posts';
 // const Block_Post = 'Post:Block_Post';
 
 const postEndPoints = {
   createPost,
+  showPost,
   editPost,
   deletePost,
-  likePost,
-  commentPost,
+  likePress,
+  makeComment,
   shareWith,
   reblogPost,
+  removeComment,
+  removeReblog,
+  getNotes,
+  getDashboard
 };
 /* =========== <--> End <--> =========== */
 
