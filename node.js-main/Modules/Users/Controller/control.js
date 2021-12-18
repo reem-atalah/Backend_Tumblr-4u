e63e1714064a67a0661d22f1059605b4d8ffae03
Rawaa Ahmed <60402572+rawaa-ahmed@users.noreply.github.com>
2021-12-19 01:52:27 +0200

    Add files via upload

--- node.js-main/Modules/Users/Controller/control.js
+++ node.js-main/Modules/Users/Controller/control.js
@@ -15,292 +15,25 @@ const schema = require('../../../Model/model');
 
 /* ----------- <---> Sign Up <---> ------- */ // *** <===> Done <===>  *** //
 // Assumption: Account Must Be Not Deleted
-
-/**
- * This Function Used To Register New User.
- *
- * @param {string} email - email
- * @param {string} password - password
- * @param {string} blogName - blogName
- * @param {string} age - age
- * @param {number} city - city
- * @param {string} country - country
- *
- * @returns {object} - { Object }
- */
-
-const signUp = async (req, res) => {
-  try {
-    const {email, password, blogName, age, city, country} = req.body;
-
-    const oldUserEmail = await schema.users.findOne({email, isDeleted: false});
-    // eslint-disable-next-line max-len
-    const oldUserBlog=await schema.users.findOne({name: blogName, isDeleted: false});
-
-    if (oldUserEmail) {
-      res.status(StatusCodes.BAD_REQUEST).json({
-        'meta': {
-          'status': 400,
-          'msg': 'BAD_REQUEST',
-        },
-
-        'res': {
-          'error': 'Email is Already Exists (<:>)',
-          'data': '',
-        },
-      });
-    // eslint-disable-next-line brace-style
-    }
-
-    // --------------- Search On Blogs Name ----------------//
-    else if (oldUserBlog) {
-      res.status(StatusCodes.BAD_REQUEST).json({
-        'meta': {
-          'status': 400,
-          'msg': 'BAD_REQUEST',
-        },
-
-        'res': {
-          'error': 'Blog Name is Already Exists (<:>)',
-          'data': '',
-        },
-      });
-    } else {
-      // eslint-disable-next-line new-cap
-      const newUser = new schema.users({
-        email,
-        password,
-        name: blogName,
-        age,
-        city,
-        country,
-      });
-      // const data = await newUser.save();
-      await newUser.save();
-
-      // --------------- Create Primary Blog ----------------//
-
-      // createBlog();
-
-      const token = jwt.sign({email, role: 'user'}, process.env.KEY);
-
-      userServices.verifyMail(blogName, email, token);
-
-      res.status(StatusCodes.CREATED).json({
-        'meta': {
-          'status': 201,
-          'msg': 'CREATED',
-        },
-
-        'res': {
-          'message': 'Sign Up Successfully (<:>)',
-          'data': token,
-        },
-      });
-    }
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      'meta': {
-        'status': 500,
-        'msg': 'INTERNAL_SERVER_ERROR',
-      },
-
-      'res': {
-        'error': 'Error In Sign Up Function (<:>)',
-        'data': '',
-      },
-    });
-  };
-};
+const signUp = require('./signup');
 
 /* ----------- <---> Sign In <---> --------- */ // *** <===> Done <===>  *** //
 // Assumption: Acount Must Be Not ( Deleted )
-
-/**
- * This Function Used To LogIn To Tumblr4U.
- *
- * @param {string} email - username
- * @param {string} password - email
- *
- * @returns {object} - { Object }
- */
-
-const login = async (req, res) => {
-  try {
-    const {email, password} = req.body;
-    const oldUser = await schema.users.findOne({email, isDeleted: false});
-    if (oldUser) {
-      const match = await bcrypt.compare(password, oldUser.password);
-      if (match) {
-        const token = jwt.sign({
-          email: oldUser.email,
-          role: oldUser.role},
-        process.env.KEY);
-        res.status(StatusCodes.OK).json({
-          'meta': {
-            'status': 200,
-            'msg': 'OK',
-          },
-
-          'res': {
-            'message': 'LogIn Successfully (<:>)',
-            'data': token,
-          },
-        });
-      } else {
-        res.status(StatusCodes.BAD_REQUEST).json({
-          'meta': {
-            'status': 400,
-            'msg': 'BAD_REQUEST',
-          },
-
-          'res': {
-            'error': 'InCorrect Password (<:>)',
-            'data': '',
-          },
-        });
-      }
-    } else {
-      res.status(StatusCodes.BAD_REQUEST).json({
-        'meta': {
-          'status': 400,
-          'msg': 'BAD_REQUEST',
-        },
-
-        'res': {
-          'error': 'Email Is Not Found (<:>)',
-          'data': '',
-        },
-      });
-    }
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      'meta': {
-        'status': 500,
-        'msg': 'INTERNAL_SERVER_ERROR',
-      },
-
-      'res': {
-        'error': 'Error In LogIn Function (<:>)',
-        'data': '',
-      },
-    });
-  };
-};
+const login = require('./login');
 
 /* ----------- <---> Verify Account <---> --- */ // *** <===> Done <===>  *** //
 // Assumption: Acount Must Be Not ( Deleted )
-
-/**
- * This Function Used To LogIn To Tumblr4U.
- *
- * @param {string} token - user secret token
- *
- * @returns {object} - { Object }
- */
-
-const verfiyAccount = async (req, res)=>{
-  try {
-    const token = req.params.token;
-    const decoded = jwt.verify(token, process.env.KEY);
-
-    const data = await schema.users
-        .updateOne({email: decoded.email}, {isVerified: true});
-    if (data.modifiedCount != 0) {
-      res.status(StatusCodes.OK).json({
-        'meta': {
-          'status': 200,
-          'msg': 'OK',
-        },
-
-        'res': {
-          'message': 'Account Verified Successfully (<:>)',
-          'data': '',
-        },
-      });
-    } else {
-      res.status(StatusCodes.BAD_REQUEST).json({
-        'meta': {
-          'status': 400,
-          'msg': 'BAD_REQUEST',
-        },
-
-        'res': {
-          'error': 'Account Is Not Found or Already Verified (<:>)',
-          'data': '',
-        },
-      });
-    }
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      'meta': {
-        'status': 500,
-        'msg': 'INTERNAL_SERVER_ERROR',
-      },
-
-      'res': {
-        'error': 'Error In Verfiy Account Function (<:>)',
-        'data': '',
-      },
-    });
-  };
-};
+const verfiyAccount = require('./verifyAccount');
 
 /* ------ <---> Sign Up With Google <---> */ // *** <===> Done <===>  *** //
 // Assumption: Acount Must Be Not ( Deleted )
+const google = require('./signupGoogle').google;
+const googleInfo = require('./signupGoogle').googleInfo;
 
-/**
- * This Function Used To LogIn To Tumblr4U.
- *
- * @param {string} email - username
- * @param {string} password - email
- *
- * @returns {object} - { Object }
- */
+/* ------ <---> Sign Up With Google For Android <---> */ // *** <===> Done <===>  *** //
+// Assumption: Acount Must Be Not ( Deleted )
+const androidSignUpWithGoogle = require('./androidSignWithGoogle');
 
-const google = async (req, res)=>{
-  try {
-    const oldUser = await schema.users.findOne({email: req.user.email});
-    console.log(oldUser);
-    if (oldUser) {
-      res.status(StatusCodes.OK).json({
-        'meta': {
-          'status': 200,
-          'msg': 'OK',
-        },
-
-        'res': {
-          'message': 'User Log In With Google Successfully (<:>)',
-          'data': req.user,
-        },
-      });
-    } else {
-      res.status(StatusCodes.OK).json({
-        'meta': {
-          'status': 200,
-          'msg': 'OK',
-        },
-
-        'res': {
-          'message': 'User Sign Up With Google Successfully (<:>)',
-          'data': req.user,
-        },
-      });
-    }
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      'meta': {
-        'status': 500,
-        'msg': 'INTERNAL_SERVER_ERROR',
-      },
-
-      'res': {
-        'error': 'Error In Sign Up With Google Function (<:>)',
-        'data': '',
-      },
-    });
-  };
-};
 // =================== End ===================//
 
 /* ------ <---> Follow Blog <---> */ // *** <===> Done <===>  *** //
@@ -376,6 +109,7 @@ const unfollowBlog = async (req) => {
   }
 };
 
+
 /* ----------- <---> Create Blog <--->  */ // *** <===> Done <===>  *** //
 
 /**
@@ -392,9 +126,15 @@ const unfollowBlog = async (req) => {
  * @returns res status and message or error massege in case of errors.
  */
 
-const createBlog = async (userId, title, name, privacy, // to be revised
-    password='password') => {
+const createBlog = async (req) => {
   try {
+    const userId=req.params.userId;
+    const name=req.body.name;
+    const title=req.body.title;
+    const privacy=req.body.privacy;
+    const password='password';
+    if(!title)
+      title='Untitled';
     let isPrimary=false;
 
     const anotherBlog= await schema.blogs.findOne({'name': name});
@@ -404,8 +144,10 @@ const createBlog = async (userId, title, name, privacy, // to be revised
       const user= await schema.users.findOne({'_id': userId}, 'blogsId name');
       if (user.blogsId.length===0) {
         isPrimary=true;
-        name=user.name;
+        privacy=false;
       }
+      if(privacy)
+      password=req.body.password;
       const blog=await schema.blogs.create(
           {
             title: title,
@@ -497,6 +239,7 @@ const deleteBlog = async (userId, blogId) => {
   }
 };
 
+
 /* =========== /// <==> End <==> ===========*/
 
 /* =============== /// <==> Export User Functions <==> /// =============== */
@@ -509,5 +252,7 @@ module.exports = {
   deleteBlog,
   verfiyAccount,
   google,
+  googleInfo,
+  androidSignUpWithGoogle
 };
 /* =========== /// <==> End <==> ===========*/
