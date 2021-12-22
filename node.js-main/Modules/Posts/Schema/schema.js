88ce34f9779f59a7bde13f5a5b793c53d44417e0
Abeerhbadr <abeer.h.badr@gmail.com>
2021-12-22 21:17:05 +0200

    soft delete

--- node.js-main/Modules/Posts/Schema/schema.js
+++ node.js-main/Modules/Posts/Schema/schema.js
@@ -37,7 +37,7 @@ const postSchema = mongoose.Schema(
 );
 
 const notesSchema = mongoose.Schema({
-  likes: { type: [String] },
+  likes: [{ type: String }],
   comments: [
     {
       commentingBlogId: { type: String },
