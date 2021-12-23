2760a3662781b13689f33fd24af946ca0e5afd16
reem <reem.atalah1@gmail.com>
2021-12-23 14:52:56 +0200

    autherization

--- Common/Middlewares/isAuthorized.js
+++ Common/Middlewares/isAuthorized.js
@@ -20,8 +20,67 @@ const userRbac = require('../Rbac/rbac');
 
 const isAuthorized = (endPoint) => {
   return async (request, response, next) => {
-    response.json({auth:request.headers.authorization,header:request.headers,res:request})
-      
+    try {
+      if (request.headers.authorization) {
+        const token = request.headers.authorization.split(' ')[1];
+        if (token) {
+          const decoded = jwt.verify(token, process.env.KEY);
+          const isAllowed=await userRbac.can(decoded.role.toString(), endPoint);
+          if (isAllowed) {
+            request.decoded = decoded;
+            next();
+          } else {
+            response.status(StatusCodes.UNAUTHORIZED).json({
+              'meta': {
+                'status': 401,
+                'msg': 'UNAUTHORIZED',
+              },
+
+              'response': {
+                'error': 'Not Authorized',
+                'data': '',
+              },
+            });
+          }
+        } else {
+          response.status(StatusCodes.UNAUTHORIZED).json({
+            'meta': {
+              'status': 401,
+              'msg': 'UNAUTHORIZED',
+            },
+
+            'response': {
+              'error': 'Token is Required',
+              'data': '',
+            },
+          });
+        }
+      } else {
+        response.status(StatusCodes.UNAUTHORIZED).json({
+          'meta': {
+            'status': 401,
+            'msg': 'UNAUTHORIZED',
+          },
+
+          'response': {
+            'error': 'Token is Required',
+            'data': '',
+          },
+        });
+      }
+    } catch (error) {
+      response.status(StatusCodes.BAD_REQUEST).json({
+        'meta': {
+          'status': 401,
+          'msg': 'UNAUTHORIZED',
+        },
+
+        'response': {
+          'error': 'Error In Is Autorized Function',
+          'data': '',
+        },
+      });
+    }
   };
 };
 /* =========== <--> End <--> =========== */
