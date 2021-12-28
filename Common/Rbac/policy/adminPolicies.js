f04afa28ef4e28a22384f36868bde51066621293
Rawaa Ahmed <60402572+rawaa-ahmed@users.noreply.github.com>
2021-12-28 16:18:25 +0200

    Update adminPolicies.js

--- Common/Rbac/policy/adminPolicies.js
+++ Common/Rbac/policy/adminPolicies.js
@@ -17,6 +17,7 @@ const adminPolicies = [
   userEndPoints.changeEmail,
   userEndPoints.followBlog,
   userEndPoints.unfollowBlog,
+  userEndPoints.doesFollow,
   userEndPoints.createBlog,
   userEndPoints.deleteBlog,
   userEndPoints.getInterests,
