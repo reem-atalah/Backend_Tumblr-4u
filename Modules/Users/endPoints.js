94a6417170dbc9be5259c8fda198f3c547b254c3
Rawaa Ahmed <60402572+rawaa-ahmed@users.noreply.github.com>
2021-12-29 00:23:54 +0200

    Update endPoints.js

--- Modules/Users/endPoints.js
+++ Modules/Users/endPoints.js
@@ -8,6 +8,7 @@ const unfollowBlog = 'User:unfollowBlog';
 const doesFollow = 'User:doesFollow';
 const createBlog = 'User:createBlog';
 const deleteBlog = 'User:deleteBlog';
+const deleteUser = 'User:deleteUser';
 const googleInfo = 'User:googleInfo';
 const changeEmail = 'User:changeEmail';
 const getInterests = 'User: getInterests';
@@ -19,6 +20,7 @@ const userEndPoints = {
   doesFollow,
   createBlog,
   deleteBlog,
+  deleteUser,
   googleInfo,
   changeEmail,
   getInterests,
