aabfddcdf24c7234b00a8fc974eaf288385a4495
Abeerhbadr <abeer.h.badr@gmail.com>
2022-01-01 19:52:42 +0200

    Posts Unit Test

--- test/postTest.js
+++ test/postTest.js
@@ -29,19 +29,19 @@ describe('Post APIs', () => {
     it('It Should Create New Post', (done) => {
       const req = {
         params: {
-          blogId: '619957113df6b45019c42d06',
+          blogId: '61c9d6b82569f9abb33ebe04',
         },
         body: {
           postHtml: '</>',
           type: 'link',
           state: 'published',
-          tags: 'tumblr',
+          tags: ['tumblr', 'cmp'],
         },
       };
 
       postFunctions.createPost(req.params.blogId, req.body.postHtml, req.body.type, req.body.state, req.body.tags).then((ret) => {
-        expect(ret).to.be.a('array');
-        expect(ret[0]).to.be.eq('Post Created Successfully');
+        expect(ret).to.be.a('string');
+        expect(ret).to.be.eq('Post Created Successfully');
         done();
       }).catch(done);
     });
@@ -50,14 +50,15 @@ describe('Post APIs', () => {
   describe('Function showPost', () => {
     it('It Should Show Post', (done) => {
       const req = {
+        url: '',
         params: {
-          postId: '61ae667d8b4d5620ce937992',
+          postId: '61ca5d91a8a4556c5b24f1f4',
         },
       };
 
-      postFunctions.showPost(req.params.postId).then((ret) => {
-        expect(ret).to.be.a('array');
-        expect(ret[0]).to.be.eq('Post Returned Successfully');
+      postFunctions.showPost(req.url,req.params.postId).then((ret) => {
+        expect(ret).to.be.a('object');
+        expect(ret.msg).to.be.eq('Post Returned Successfully');
         done();
       }).catch(done);
     });
@@ -67,8 +68,8 @@ describe('Post APIs', () => {
     it('It Should Make Comment', (done) => {
       const req = {
         params: {
-          blogId: '619957113df6b45019c42d06',
-          postId: '61ae667d8b4d5620ce937992',
+          blogId: '61c9d6b82569f9abb33ebe04',
+          postId: '61ca5d91a8a4556c5b24f1f4',
         },
         body: {
           text: 'Very Good Post',
@@ -76,8 +77,8 @@ describe('Post APIs', () => {
       };
 
       postFunctions.makeComment(req.params.blogId, req.params.postId, req.body.text).then((ret) => {
-        expect(ret).to.be.a('array');
-        expect(ret[0]).to.be.eq('Comment Posted Successfully');
+        expect(ret).to.be.a('string');
+        expect(ret).to.be.eq('Comment Posted Successfully');
         done();
       }).catch(done);
     });
@@ -87,8 +88,8 @@ describe('Post APIs', () => {
     it('It Should Remove a Comment', (done) => {
       const req = {
         params: {
-          postId: '61ae667d8b4d5620ce937992',
-          commentId: '61b50d9837c346ee39708965',
+          postId: '61ca5d91a8a4556c5b24f1f4',
+          commentId: '61d09371d53d981f8a647ef6',
         },
       };
 
@@ -105,8 +106,8 @@ describe('Post APIs', () => {
     it('It Should Reblog a Post', (done) => {
       const req = {
         params: {
-          blogId: '619957113df6b45019c42d06',
-          postId: '61ae667d8b4d5620ce937992',
+          blogId: '61c9d6b82569f9abb33ebe04',
+          postId: '61ca5d91a8a4556c5b24f1f4',
         },
         body: {
           text: 'Very Good Post',
@@ -115,8 +116,8 @@ describe('Post APIs', () => {
 
       postFunctions.reblogPost(req.params.blogId, req.params.postId, req.body.text)
           .then((ret) => {
-            expect(ret).to.be.a('array');
-            expect(ret[0]).to.be.eq('Post Reblogged Successfully');
+            expect(ret).to.be.a('string');
+            expect(ret).to.be.eq('Post Reblogged Successfully');
             done();
           }).catch(done);
     });
@@ -126,8 +127,8 @@ describe('Post APIs', () => {
     it('It Should Remove a Reblog', (done) => {
       const req = {
         params: {
-          postId: '61ae667d8b4d5620ce937992',
-          reblogId: '61b51524807f8f7c61e5c14b',
+          postId: '61ca5d91a8a4556c5b24f1f4',
+          reblogId: '61d09372d53d981f8a647f43',
         },
       };
 
@@ -140,12 +141,20 @@ describe('Post APIs', () => {
     });
   });
 
-  describe('Function loopAndCheck', () => {
+  describe('Function loopLikeAndCheck', () => {
     it('It Should Loop on an Array and Check if an Element Exists', (done) => {
-      const arr = [0, 1, 2];
-      const element = 1;
-      // expect(postControl.loopAndCheck(arr, element)).to.be.equal(1);
-      postFunctions.loopAndCheck(arr, element).should.be.eq(1);
+      const a = '61af36a793d7c3194ff42269';
+      const b = '61ae881aab9ea73f2282b03e';
+      const c = '61ae887d2676ed5d9b5d532f';
+      const arr = [
+        {isDeleted: false, blogId: a},
+        {isDeleted: false, blogId: b},
+        {isDeleted: false, blogId: c},
+      ];
+      const element = '61ae881aab9ea73f2282b03e';
+      postFunctions.loopLikeAndCheck(arr, element)[0].should.be.eq(1);
+      postFunctions.loopLikeAndCheck(arr, element)[1].should.be.eq(0);
+      postFunctions.loopLikeAndCheck(arr, element)[2].should.be.eq(1);
       done();
     });
   });
@@ -156,9 +165,9 @@ describe('Post APIs', () => {
       const b = mongoose.Types.ObjectId('61ae881aab9ea73f2282b03e');
       const c = mongoose.Types.ObjectId('61ae887d2676ed5d9b5d532f');
       const arr = [
-        {text: 'a', _id: a},
-        {text: 'shd', _id: b},
-        {text: 'akj', _id: c},
+        {isDeleted: false, _id: a},
+        {isDeleted: false, _id: b},
+        {isDeleted: false, _id: c},
       ];
       const element = '61ae881aab9ea73f2282b03e';
       postFunctions.loopObjAndCheck(arr, element)[0].should.be.eq(1);
@@ -171,8 +180,8 @@ describe('Post APIs', () => {
     it('It Should Like or Unlike a Post', (done) => {
       const req = {
         params: {
-          blogId: '619957113df6b45019c42d06',
-          postId: '61ae667d8b4d5620ce937992',
+          blogId: '61c9d6b82569f9abb33ebe04',
+          postId: '61ca5d91a8a4556c5b24f1f4',
         },
       };
 
@@ -188,13 +197,13 @@ describe('Post APIs', () => {
     it('It Should Get Post Notes', (done) => {
       const req = {
         params: {
-          postId: '61ae667d8b4d5620ce937992',
+          notesId: '61ca5d92a8a4556c5b24f1f9',
         },
       };
 
-      postFunctions.getNotes(req.params.postId).then((ret) => {
-        expect(ret).to.be.a('array');
-        expect(ret[0]).to.be.eq('Notes Got Successfully');
+      postFunctions.getNotes(req.params.notesId).then((ret) => {
+        expect(ret).to.be.a('object');
+        expect(ret.msg).to.be.eq('Notes Got Successfully');
         done();
       }).catch(done);
     });
@@ -203,18 +212,118 @@ describe('Post APIs', () => {
   describe('Function getDashboard', () => {
     it('It Should Get Dashboard', (done) => {
       const req = {
-        params: {
-          userId: '6196d197a31552477d437404',
-          blogId: '619957113df6b45019c42d06',
+        decoded: {
+          email: 'ahmed.ayman.1420@gmail.com'
         },
       };
 
-      postFunctions.getDashboard(req.params.userId, req.params.blogId).then((ret) => {
-        expect(ret).to.be.a('array');
-        expect(ret[0]).to.be.eq('Dashboard Got Successfully');
+      postFunctions.getDashboard(req.decoded.email).then((ret) => {
+        expect(ret).to.be.a('object');
+        expect(ret.msg).to.be.eq('Dashboard Got Successfully');
         done();
       }).catch(done);
     });
   });
+
+  describe('Function editPost', () => {
+    it('It Should Edit a Post Post', (done) => {
+      const req = {
+        params: {
+          postId: '61cd7bd4c53c43b422960a2c',
+        },
+        body: {
+          postHtml: '</>',
+          tags: ['tumblr', 'cmp'],
+        },
+      };
+
+      postFunctions.editPost(req.params.postId, req.body.postHtml, req.body.tags).then((ret) => {
+        expect(ret).to.be.a('string');
+        expect(ret).to.be.eq('Post Edited Successfully');
+        done();
+      }).catch(done);
+    });
+  });
+
+  describe('Function reportPost', () => {
+    it('It Should Report a Post', (done) => {
+      const req = {
+        params: {
+          postId: '61cd7bd4c53c43b422960a2c',
+        },
+      };
+
+      postFunctions.reportPost(req.params.postId).then((ret) => {
+        expect(ret).to.be.a('string');
+        expect(ret).to.be.eq('Post Reported Successfully');
+        done();
+      }).catch(done);
+    });
+  });
+
+  describe('Function deletePost', () => {
+    it('It Should Delete a Post', (done) => {
+      const req = {
+        params: {
+          postId: '61d09370d53d981f8a647ecc',
+        },
+      };
+
+      postFunctions.deletePost(req.params.postId).then((ret) => {
+        expect(ret).to.be.a('string');
+        expect(ret).to.be.eq('Post Deleted Successfully');
+        done();
+      }).catch(done);
+    });
+  });
+
+  describe('Function getBlogPosts', () => {
+    it('It Should Blog Posts', (done) => {
+      const req = {
+        params: {
+          blogId: '61c9d6b82569f9abb33ebe04'
+        },
+      };
+
+      postFunctions.getBlogPosts(req.params.blogId).then((ret) => {
+        expect(ret).to.be.a('object');
+        expect(ret.msg).to.be.eq('Blog Posts Got Successfully');
+        done();
+      }).catch(done);
+    });
+  });
+
+  describe('Function getLikedPosts', () => {
+    it('It Should Get Liked Posts', (done) => {
+      const req = {
+        params: {
+          blogId: '61c9d6b82569f9abb33ebe04'
+        },
+      };
+
+      postFunctions.getLikedPosts(req.params.blogId).then((ret) => {
+        expect(ret).to.be.a('object');
+        expect(ret.msg).to.be.eq('Liked Posts Got Successfully');
+        done();
+      }).catch(done);
+    });
+  });
+
+  describe('Function activityFeed', () => {
+    it('It Should Get Activity Feed', (done) => {
+      const req = {
+        params: {
+          blogId: '61c9d6b82569f9abb33ebe04'
+        },
+      };
+
+      postFunctions.activityFeed(req.params.blogId).then((ret) => {
+        expect(ret).to.be.a('object');
+        expect(ret.msg).to.be.eq('Activity Feed Got Successfully');
+        done();
+      }).catch(done);
+    });
+  });
+
 });
 /* =========== /// <==> End <==> ===========*/
